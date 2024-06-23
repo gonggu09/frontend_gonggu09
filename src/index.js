72dfe8d3305fbb4280fa1f22b6e9e5f5e142dfb0
이형빈 <ihyeongbin@ihyeongbin-ui-MacBookPro.local>
2024-06-23 14:59:42 +0900

    폰트 적용

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GlobalStyle } from "./styles/GlobalStyles"; // GlobalStyle 불러오기
-import "./styles/font.css"; // 폰트 스타일 불러오기 (있는 경우)
-
+import { GlobalStyle } from "./styles/GlobalStyles";
+import "./styles/font.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <GlobalStyle /> {/* GlobalStyle 적용 */}
+    <GlobalStyle />
     <App />
   </>
 );
